f00d5d26b6914490294c8abe1a0afffbca023744
mrStha <shrs_samar@yahoo.com>
2017-02-12 00:17:25 +0200

    add existing files

---
+++ test.py
@@ -0,0 +1,17 @@
+from sys import argv
+script,first, second , third = argv
+print " the script is ", script
+
+print 'helo'
+x = 'there are %d types of people' %10
+x2 = 'there are %d types of people'
+
+print  '%r' %x
+print '%s' %x
+print "'%s'"%x
+
+age = raw_input('enter age ')
+ht = raw_input('enter ht')
+
+
+print " age = %r ht=%r" %(age, ht)

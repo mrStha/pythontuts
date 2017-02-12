3275e96a7df3f146a0d33d5948205d45eb528a20
mrStha <shrs_samar@yahoo.com>
2017-02-12 16:29:55 +0200

    more updates for rcp

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,22 +1,37 @@
 import sys
-"""
-user1 = input("input your name please")
-user2 = input("input your name please")
-
-print ("hello %s and %s" %(user1, user2))
-choice1 = input("%s, choose one: rock paper scissors" % user1)
-choice2 =input ("%s, choose one:rock paper scissors" % user2)
-"""
 
 
-user1 =raw_input("What's your name?")
-user2 = raw_input("And your name?")
-choice1 = raw_input("%s, do yo want to choose rock, paper or scissors?" % user1)
-choice2 = raw_input("%s, do you want to choose rock, paper or scissors?" % user2)
 
-if((choice1=="rock" and choice2=="scissors") or (choice1=="scissors" and choice2=="paper") or (choice1=="paper" and choice2=="rock")):
-    print("%s is winner" %user1) 
-elif(choice1==choice2):
-    print("Sorry its a tie")
-else: 
-    print("\n%s is winner" %user2)
+def game(play_again):
+       
+        if(play_again==True):
+            user1 =raw_input("What's your name?\t")
+            user2 = raw_input("And your name?\t")
+            choice1 = raw_input("%s, do yo want to choose rock, paper or scissors?\t" % user1)
+            choice2 = raw_input("%s, do you want to choose rock, paper or scissors?\t" % user2)
+
+            if((choice1=="rock" and choice2=="scissors") or (choice1=="scissors" and choice2=="paper") or (choice1=="paper" and choice2=="rock")):
+                print("\nCongratulations! %s is winner\n" %user1) 
+                moregame()
+            elif(choice1==choice2):
+                print("\nSorry its a tie\n")
+                moregame()
+            elif((choice2=="rock" and choice1=="scissors") or (choice2=="scissors" and choice1=="paper") or (choice2=="paper" and choice1=="rock")): 
+                print("\nCongratulations %s is winner\n" %user2)
+                moregame()
+            else:
+                print(" choice could not be identified\n")
+                moregame()
+            
+
+def moregame():            
+        play = raw_input(" Continue again (Y/N)? ")
+        if(play=="Y" or play =="yes" or play =="y"): 
+            play_again = True
+            game(play_again);
+
+        else: 
+            play_again = False            
+            game(play_again)
+
+game(True);
---
+++ whiletest.py
@@ -0,0 +1,6 @@
+import sys
+count = 0
+
+while (count<9):
+    print "%d " %(count+1)
+    count+=1
